954730f0418377d775a058c40ac79d14547a3dbb
Immerfallen <marodemelo10@gmail.com>
2021-07-02 16:00:19 -0300

    criadas classes series e entidade base

--- Classes/Series.cs
+++ Classes/Series.cs
@@ -1,12 +1,39 @@
+using System;
+
 namespace projeto
 {
     public class Series : EntidadeBase
     {
-        private Genero genero {get;set;}
+        private Genero Genero {get;set;}
         private string Titulo {get;set;}
         private string Descricao { get; set; }
         private int Ano {get;set;}
         
-        
+        public Series(int id, Genero genero, string titulo, string descricao, int ano){
+            this.Id =id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao =descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Genero: " +this.Genero + Environment.NewLine;
+            retorno += "Titulo: " +this.Titulo + Environment.NewLine;
+            retorno += "Descricao: " +this.Descricao + Environment.NewLine;
+            retorno += "Ano: " +this.Ano + Environment.NewLine;
+
+            return retorno;
+        }
+
+        public string retornaTitulo(){
+            return this.Titulo;
+        }
+
+        public string retornaId(){
+            return this.Id;
+        }
     }
 }
